7eea31b03cd173e686d6fb3ca2a5742c8267b979
Elias288 <bianchi.elias@gmail.com>
2026-05-08 14:13:06 -0300

    style: Muestra de información

--- src/app/routes/info.tsx
+++ src/app/routes/info.tsx
@@ -3,8 +3,8 @@ import type { Route } from "../+types/root";
 import Spinner from "@/icons/spinner";
 import { Navigate } from "react-router";
 import { useEffect, useState, type ReactNode } from "react";
-import Header, { NavBar } from "@/layout/header";
-import type { MultimediaTypes, Status } from "@/types/data.type";
+import { NavBar } from "@/layout/header";
+import type { Status } from "@/types/data.type";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,7 +15,6 @@ export function meta({}: Route.MetaArgs) {
 
 const Info = () => {
   const { data, status, setLoaded } = useMediaContext();
-  const [showSideBar, setShowSideBar] = useState<boolean>(false);
   const [categoriesCount, setCategoriesCount] =
     useState<Record<string, number>>();
   const [statusCount, setStatusCount] = useState<Record<Status, number>>();
@@ -43,8 +42,6 @@ const Info = () => {
     }
   }, []);
 
-  const toggleSideBar = () => setShowSideBar(!showSideBar);
-
   const Block = ({ title, value }: { title: string; value: number }) => {
     return (
       <div className="bg-gray-700 rounded-lg p-2 flex-1">
@@ -64,40 +61,33 @@ const Info = () => {
     );
 
   return (
-    <div
-      className={`min-h-screen min-w-screenMinWidth md:grid duration-100 ease-in-out grid-rows-[auto_1fr] ${showSideBar ? "md:grid-cols-[350px_auto]" : "md:grid-cols-[0_auto] "}`}
-    >
-      <Header sideBarOpen={showSideBar} toggleSideBar={toggleSideBar} />
-      <SideBar isOpen={showSideBar} toggleSideBar={toggleSideBar} />
+    <MainContainer>
+      <div className="bg-background2 rounded-lg px-5 pt-5 pb-10 grid md:grid-cols-2 gap-5 md:gap-x-20">
+        <h2 className="text-2xl col-span-full">Información</h2>
 
-      <MainContainer>
-        <div className="bg-background2 rounded-lg p-5 py-10 grid md:grid-cols-2 gap-5 md:gap-x-20">
-          <h2 className="text-2xl col-span-full">Información</h2>
+        {categoriesCount && (
+          <section className="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-2">
+            <h3 className="col-span-full">Categorías</h3>
 
-          {categoriesCount && (
-            <section className="grid grid-cols-2 md:grid-cols-4 gap-2">
-              <h3 className="col-span-2 md:col-span-full">Categorías</h3>
+            <Block title="Animes" value={categoriesCount["anime"]} />
+            <Block title="Series" value={categoriesCount["serie"]} />
+            <Block title="Mangas" value={categoriesCount["manga"]} />
+            <Block title="Comics" value={categoriesCount["comic"]} />
+          </section>
+        )}
 
-              <Block title="Animes" value={categoriesCount["anime"]} />
-              <Block title="Series" value={categoriesCount["serie"]} />
-              <Block title="Mangas" value={categoriesCount["manga"]} />
-              <Block title="Comics" value={categoriesCount["comic"]} />
-            </section>
-          )}
+        {statusCount && (
+          <section className="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-2">
+            <h3 className="col-span-full">Estados</h3>
 
-          {statusCount && (
-            <section className="grid grid-cols-2 md:grid-cols-4 gap-2">
-              <h3 className="col-span-2 md:col-span-full">Estados</h3>
-
-              <Block title="Por ver" value={statusCount["por ver"]} />
-              <Block title="Vistos" value={statusCount["visto"]} />
-              <Block title="Viendo" value={statusCount["viendo"]} />
-              <Block title="Dejado" value={statusCount["dejado"]} />
-            </section>
-          )}
-        </div>
-      </MainContainer>
-    </div>
+            <Block title="Por ver" value={statusCount["por ver"]} />
+            <Block title="Vistos" value={statusCount["visto"]} />
+            <Block title="Viendo" value={statusCount["viendo"]} />
+            <Block title="Dejado" value={statusCount["dejado"]} />
+          </section>
+        )}
+      </div>
+    </MainContainer>
   );
 };
 
