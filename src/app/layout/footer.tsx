ed75a5e42ec1a57ba007226093b38eced3d2df6a
Elias288 <bianchi.elias@gmail.com>
2026-05-09 10:16:16 -0300

    feat: Usando versión de Package.json

--- src/app/layout/footer.tsx
+++ src/app/layout/footer.tsx
@@ -2,7 +2,9 @@ const Footer = () => {
   return (
     <footer className="p-4 col-span-full">
       <p className="text-center text-gray-500">Hecho por Elias</p>
-      <small className="block text-center text-gray-500">v.1.0.1</small>
+      <small className="block text-center text-gray-500">
+        v{__APP_VERSION__}
+      </small>
     </footer>
   );
 };
--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,10 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import pkg from "./package.json";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
   },
 });
